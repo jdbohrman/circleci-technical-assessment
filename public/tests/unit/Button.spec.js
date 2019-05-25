02ce9441440b93f8f3fc68c77ea635ba87bd7446
James D. Bohrman <jdbohrman@outlook.com>
2019-05-25 01:48:01 -0500

    Tests

--- public/tests/unit/Button.spec.js
+++ public/tests/unit/Button.spec.js
@@ -1,13 +1,15 @@
-import Button from '../components/Button.vue'
+import { expect } from 'chai'
 import { mount } from '@vue/test-utils'
+import Button from '@/components/Button.vue'
 import sinon from 'sinon'
 
-describe('Click event', () => {
-  it('Click on answer button calls our method with argument "answer"', () => {
+
+describe('Button was clicked', () => {
+  it('Clicked button calls our method with argument "answer"', () => {
     const spy = sinon.spy()
     const wrapper = mount(Button, {
       propsData: {
-        alert: spy
+        answer: spy
       }
     })
     wrapper.find('button.answer').trigger('click')
