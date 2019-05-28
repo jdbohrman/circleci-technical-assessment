7f0f3fc3a528d70f50706adf7e72cf403c6eb03c
James D. Bohrman <jdbohrman@outlook.com>
2019-05-28 12:44:46 -0500

    new tests

--- tests/unit/Button.spec.js
+++ tests/unit/Button.spec.js
@@ -1,19 +1,23 @@
-import { expect } from 'chai'
+import chai from 'chai'
 import { mount } from '@vue/test-utils'
 import Button from '@/components/Button.vue'
 import sinon from 'sinon'
+import sinonChai from 'sinon-chai'
+
+chai.should()
+chai.use(sinonChai)
 
 
 describe('Button was clicked', () => {
   it('Clicked button calls our method with argument "answer"', () => {
     const spy = sinon.spy()
     const wrapper = mount(Button, {
-      listeners: {
-        Button: spy
+      methods: {
+        answer: spy
       }
     })
     wrapper.find('button').trigger('click')
-
-    spy.should.have.been.calledWith('answer')
+spy.should.have.been.calledWith('answer')
+        spy.should.have.been.called
   })
 })
